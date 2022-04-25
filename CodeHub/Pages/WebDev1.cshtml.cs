c7294fc89d8de485ed46e2ff2167631ef74284e6
nasyav <nv339@nau.edu>
2022-04-25 11:30:10 -0700

    new tabs and fixed computer literacy test

---
+++ CodeHub/Pages/WebDev1.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CodeHub.Pages
+{
+    public class WebDev1Model : PageModel
+    {
+        private readonly ILogger<WebDev1Model> _logger;
+
+        public WebDev1Model(ILogger<WebDev1Model> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
